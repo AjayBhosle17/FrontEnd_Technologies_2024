4e470d8fb75d0106a68480ad8adbea1420a6ba65
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-10-25 13:00:33 +0530

    Traversing

--- JQuery/7_traversing/traverse.js
+++ JQuery/7_traversing/traverse.js
@@ -2,7 +2,9 @@
 $(function(){
 
     $('#btn1').click(function(){
-       
+
+        //parent
+
         // $('#mainDiv').parent().css({
         //     'color':'red',
         //     'border':'2px solid red'
@@ -18,10 +20,67 @@ $(function(){
         //     'border':'2px solid blue'
         // });
     
-        $('h3').parents().css({
-            'color':'blue',
-            'border':'2px solid blue'
-        });
+        // $('h3').parents().css({
+        //     'color':'blue',
+        //     'border':'2px solid blue'
+        // });
     
+        // Child
+
+        // $('#Div1').children().css({
+
+        //     'color':'red',
+        //     'border':'2px solid blue'
+        // });
+
+        // $('#mainDiv').find('p').css({
+
+        //     'color':'red',
+        //     'border':'2px solid blue'
+        // });
+
+
+        // sibling
+
+        // $('#Div1').siblings().css({
+
+        //     'color':'red',
+        //     'border':'2px solid yellow'
+        // });
+
+        // $('h5').prev().css({
+
+        //     'color':'red',
+        //     'border':'2px solid green'
+        // });
+
+        // $('p').next().css({
+
+        //     'color':'red',
+        //     'border':'2px solid pink'
+        // });
+
+        // $('h5').prevAll().css({
+
+        //     'color':'red',
+        //     'border':'2px solid pink'
+        // });
+
+        // $('h5').prevUntil('#mainDiv').css({
+
+        //     'color':'red',
+        //     'border':'2px solid pink'
+        // });
+
+        // $('p').nextAll().css({
+        //     'color':'red',
+        //     'border':'2px solid pink'
+                
+        // })
+        $('p').nextUntil('h5').css({
+            'color':'red',
+            'border':'2px solid pink'
+          
+        })
     });
 });
